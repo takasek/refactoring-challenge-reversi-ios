41f4907c6fbb3fd6d4ca5591288e12e7f4fe6474
takasek <takassekiyoshi@gmail.com>
2020-05-16 18:16:03 +0900

    BoardViewの計算メソッドのテストを書いた

--- Reversi/StateTests.swift
+++ Reversi/StateTests.swift
@@ -51,4 +51,81 @@ class StateTests: XCTestCase {
 
         XCTAssertEqual(state1, state2)
     }
+
+    func test_countDisks() {
+        // TODO: Stateのメソッドとする
+        let v = BoardView()
+        try! v.applyWithoutAnimation(State(_10_10_and: "------x-").board)
+
+        XCTAssertEqual(v.countDisks(of: .light), 10)
+        XCTAssertEqual(v.countDisks(of: .dark), 11)
+    }
+
+    func test_sideWithMoreDisks() {
+        // TODO: Stateのメソッドとする
+        let v = BoardView()
+        try! v.applyWithoutAnimation(State(_10_10_and: "--------").board)
+        XCTAssertEqual(v.sideWithMoreDisks(), nil)
+
+        try! v.applyWithoutAnimation(State(_10_10_and: "-------o").board)
+        XCTAssertEqual(v.sideWithMoreDisks(), .light)
+
+        try! v.applyWithoutAnimation(State(_10_10_and: "-------x").board)
+        XCTAssertEqual(v.sideWithMoreDisks(), .dark)
+    }
+
+    func test_canPlaceDisk() {
+        // TODO: Stateのメソッドとする
+        let v = BoardView()
+        try! v.applyWithoutAnimation(State(input: """
+            -01
+            x-------
+            -o------
+            --------
+            --------
+            --------
+            --------
+            --------
+            --------\n
+            """)!.board)
+
+        XCTAssertEqual(v.canPlaceDisk(.light, atX: 2, y: 1), false)
+        XCTAssertEqual(v.canPlaceDisk(.light, atX: 2, y: 2), false)
+        XCTAssertEqual(v.canPlaceDisk(.dark, atX: 2, y: 2), true)
+    }
+
+    func test_validMoves() {
+        // TODO: Stateのメソッドとする
+        let v = BoardView()
+        try! v.applyWithoutAnimation(State(input: """
+            -01
+            x-------
+            -o------
+            --------
+            --------
+            --------
+            --------
+            --------
+            --------\n
+            """)!.board)
+
+        XCTAssertEqual(v.validMoves(for: .light), [])
+        XCTAssertEqual(v.validMoves(for: .dark), [.init(x: 2, y: 2)])
+    }
+}
+
+extension State {
+    init(_10_10_and lastLine: String) {
+        self.init(input: """
+        -01
+        --------
+        --------
+        xxxxoooo
+        ooooxxxx
+        ----oo--
+        --------
+        ------xx
+        \n
+        """ + lastLine)!
+    }
 }
